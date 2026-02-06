2112e7510c1a910d18b12dd51a65884acb7546f3
Khalifa Alhasan <khlfaalhsn5@gmail.com>
2026-02-06 16:17:42 +0700

    refactor: move server implementation to api package and update config

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,93 +1,71 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"github.com/khalifaalhasan/go-gin-clean-arch/internal/config"
 	"github.com/khalifaalhasan/go-gin-clean-arch/internal/database"
-	"github.com/khalifaalhasan/go-gin-clean-arch/internal/handler"
-	"github.com/khalifaalhasan/go-gin-clean-arch/internal/middleware"
-	"github.com/khalifaalhasan/go-gin-clean-arch/internal/service"
+	
+	"github.com/khalifaalhasan/go-gin-clean-arch/api"
 )
+	
 
 func main() {
+	// 1. Load config
 	cfg, err := config.LoadConfig(".")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
 
 	// 2. Konek Database
-	conn, err := database.NewConnection(cfg.DBSource)
+	conn, err := sql.Open(cfg.DBDriver, cfg.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	
 	defer conn.Close()
 
+	// Tes Koneksi DB
+	if err:= conn.Ping(); err != nil {
+		log.Fatal("cannot ping db:", err)
+	}
+	log.Println("Database Connected successfully!")
 
+	// Initialize database store (SQLC)
 	store := database.New(conn)
 
-	server := NewServer(store)
+	// Konek Redis
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+	}
 
+	rdb := redis.NewClient(&redis.Options{
+		Addr: cfg.RedisAddress,
+		Password: cfg.RedisPassword,
+		DB : 0,
+	})
+
+	// tes koneksi redis
+	if _, err := rdb.Ping(rdb.Context()).Result(); err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	log.Println("Redis connected successfully!")
+
+	// Buat Server 
+	server, err := api.NewServer(cfg, store, conn, rdb)
+	if err != nil{
+		log.Fatal("cannot create server:", err)
+	}
+
+	// Run Server
+	log.Printf("Starting server on %s", cfg.ServerAddress)
 	err = server.Start(cfg.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
 	
+}	
 
-	log.Println("Database connected successfully!")
-	
-	redisAddr := os.Getenv("REDIS_ADDR")
-	if redisAddr == "" {
-		redisAddr = "localhost:6379"
-	}
-	turnstileSecret := os.Getenv("TURNSTILE_SECRET_KEY")
-	if turnstileSecret == "" {
-		turnstileSecret = "1x0000000000000000000000000000000AA" // 
-	}
-
-
-	rdb := redis.NewClient(&redis.Options{
-		Addr: redisAddr,
-	})
-	
-	if _, err := rdb.Ping(rdb.Context()).Result(); err != nil {
-		log.Fatalf("Could not connect to Redis: %v", err)
-	}
-
-	
-	secService := service.NewSecurityService(turnstileSecret)
-	commentHandler := handler.NewCommentHandler(secService)
-
-
-	r := gin.Default() 
-	
-
-
-r.Use(cors.New(cors.Config{
-    AllowOrigins:     []string{"http://localhost:3000"}, // Izinkan Next.js
-    AllowMethods:     []string{"POST", "OPTIONS"},
-    AllowHeaders:     []string{"Origin", "Content-Type", "X-RateLimit-Remaining"},
-    ExposeHeaders:    []string{"Content-Length"},
-    AllowCredentials: true,
-}))
-	
-	
-
-
-	api := r.Group("/api/v1")
-	{
-		
-		api.POST("/comments", middleware.RateLimiter(rdb), commentHandler.PostComment)
-	}
-
-	
-	log.Println("Server running on port 8080")
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
-	}
-}
--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -6,9 +6,15 @@ import (
 
 // Config menyimpan semua konfigurasi aplikasi
 type Config struct {
+	DBDriver	 string `mapstructure:"DB_DRIVER"`
 	DBSource      string `mapstructure:"DB_SOURCE"`
 	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
 	GinMode       string `mapstructure:"GIN_MODE"`
+
+	RedisAddress string `mapstructure:"REDIS_ADDRESS"`
+	RedisPassword string `mapstructure:"REDIS_PASSWORD"`
+	
+	TurnstileSecret string `mapstructure:"TURNSTILE_SECRET_KEY"`
 }
 
 // LoadConfig membaca konfigurasi dari file ATAU environment variable
